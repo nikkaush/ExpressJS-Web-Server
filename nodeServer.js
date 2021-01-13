72287cf7deda8d7b03336da5c4ed72e1554f0960
Nikhil Kaushik <nk2214@rit.edu>
2021-01-12 21:23:13 -0500

    nodeServer.js add routes to node web-server

--- nodeServer.js
+++ nodeServer.js
@@ -1,18 +1,30 @@
 const http = require("http");
 
+// file system module
+// fs give node, access to this file system
+const fs = require("fs");
+
 // http module has a createServer method
 // takes 1 argument that is a callback
 // the callback has 2 args: req, res object
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
   // res = our way of responding to the request
 
-  // writeHead takes in two arguments, status code and object for the mime-type
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Hello world!</h1>");
-  res.end();
-  // console.log(res);
+  // adding routes to node webServer
+  if (req.url === "/") {
+    // writeHead takes in two arguments, status code and object for the mime-type
+    res.writeHead(200, { "content-type": "text/html" });
+    // res.write("<h1>This is the home page./h1>");
+    const homePageHTML = fs.readFileSync("node.html");
+    res.write(homePageHTML);
+    res.end();
+  } else {
+    // writeHead takes in two arguments, status code and object for the mime-type
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h3>Sorry this isn't the page you are looking for!</h3>");
+    res.end();
+  }
 });
 
 // createServer returns an object with listen method
